804f294db6c9d0a17a11b34a1d53c7691690230a
enjhnsn2 <e5johnso@eng.ucsd.edu>
2024-02-12 12:12:33 -0800

    add initial version of libraries

---
+++ src/xy.rs
@@ -0,0 +1,157 @@
+use std::{
+    fmt::{self, Formatter},
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
+
+#[derive(Clone, PartialEq, Copy)]
+pub struct XYArr {
+    inner: [f64; 2],
+}
+
+impl XYArr {
+    pub fn new(inner: [f64; 2]) -> Self {
+        Self { inner }
+    }
+
+    pub fn zeroes() -> Self {
+        XYArr { inner: [0.0; 2] }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.inner[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.inner[1]
+    }
+
+    pub fn scale_by(&self, d: f64) -> Self {
+        let mut r = Self::zeroes();
+        r.inner[0] = self.inner[0] * d;
+        r.inner[1] = self.inner[1] * d;
+        r
+    }
+
+    pub fn div_by(&self, d: f64) -> Self {
+        let mut r = Self::zeroes();
+        r.inner[0] = self.inner[0] / d;
+        r.inner[1] = self.inner[1] / d;
+        r
+    }
+
+    // // pick XYArr from normal distribution
+    // pub fn random(rng: &mut SmallRng) -> Self {
+    //     let normal = Normal::new(0.0, 1.0).unwrap();
+    //     let mut r = XYArr::zeroes();
+    //     for idx in 0..2 {
+    //         r.inner[idx] = normal.sample(rng);
+    //     }
+    //     r
+    // }
+
+    // // normalized radom XYArr
+    // pub fn rand_unit_vector(rng: &mut SmallRng) -> Self {
+    //     let rand_pos = XYArr::random(rng);
+    //     let norm = rand_pos.norm();
+    //     rand_pos.scalar_div(norm)
+    // }
+
+    pub fn l1_norm(&self) -> f64 {
+        self.x() + self.y()
+    }
+
+    pub fn l2_norm(&self) -> f64 {
+        self.l2_norm_sqd().sqrt()
+    }
+
+    pub fn l2_norm_sqd(&self) -> f64 {
+        self.x() * self.x() + self.y() * self.y()
+    }
+
+    pub fn cross_prod(&self, other: XYArr) -> f64 {
+        self.x() * other.y() - self.y() * other.x()
+    }
+
+    pub fn dot_prod(&self, other: XYArr) -> f64 {
+        self.x() * other.x() + self.y() * other.y()
+    }
+
+    pub fn translated_by(&self, x: f64, y: f64) -> Self {
+        let new_x = self.x() + x;
+        let new_y = self.y() + y;
+        Self {
+            inner: [new_x, new_y],
+        }
+    }
+
+    // TODO: what are the units of theta? (update docs if its not radians)
+    pub fn rotated_by(&self, theta: f64) -> Self {
+        let c = theta.cos();
+        let s = theta.sin();
+
+        let x = (self.x() * c - self.y() * s) - self.x();
+        let y = self.x() * s + self.y() * c - self.y();
+        Self::new([x, y])
+    }
+
+    // // TODO: should probably generalize this
+    // pub fn div_u32(self, other: [u32; 2]) -> Self {
+    //     let mut r = XYArr::zeroes();
+    //     for idx in 0..2 {
+    //         r.inner[idx] = self.inner[idx] / other[idx] as f64;
+    //     }
+    //     r
+    // }
+}
+
+impl Add for XYArr {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let mut r = XYArr::zeroes();
+        for idx in 0..2 {
+            r.inner[idx] = self.inner[idx] + other.inner[idx];
+        }
+        r
+    }
+}
+
+impl AddAssign for XYArr {
+    fn add_assign(&mut self, other: XYArr) {
+        for idx in 0..2 {
+            self.inner[idx] += other.inner[idx];
+        }
+    }
+}
+
+impl Sub for XYArr {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        let mut r = XYArr::zeroes();
+        for idx in 0..2 {
+            r.inner[idx] = self.inner[idx] - other.inner[idx];
+        }
+        r
+    }
+}
+
+impl SubAssign for XYArr {
+    fn sub_assign(&mut self, other: XYArr) {
+        for idx in 0..2 {
+            self.inner[idx] -= other.inner[idx];
+        }
+    }
+}
+
+impl fmt::Debug for XYArr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "({:?}, {:?})", self.x(), self.y())
+    }
+}
+
+impl fmt::Display for XYArr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "({:.3}, {:.3})", self.x(), self.y())
+    }
+}
---
+++ src/xyz.rs
@@ -0,0 +1,150 @@
+use std::{
+    fmt::{self, Formatter},
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
+
+// TODO: parameterize by floating point length / fixed-point
+// TODO: simd support?
+
+#[derive(Clone, PartialEq, Copy)]
+pub struct XYZArr {
+    inner: [f64; 3],
+}
+
+impl XYZArr {
+    pub fn new(inner: [f64; 3]) -> Self {
+        Self { inner }
+    }
+
+    pub fn zeroes() -> Self {
+        Self { inner: [0.0; 3] }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.inner[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.inner[1]
+    }
+
+    pub fn z(&self) -> f64 {
+        self.inner[2]
+    }
+
+    pub fn scale_by(&self, d: f64) -> Self {
+        let mut r = Self::zeroes();
+        r.inner[0] = self.inner[0] * d;
+        r.inner[1] = self.inner[1] * d;
+        r.inner[2] = self.inner[2] * d;
+        r
+    }
+
+    pub fn div_by(&self, d: f64) -> Self {
+        let mut r = Self::zeroes();
+        r.inner[0] = self.inner[0] / d;
+        r.inner[1] = self.inner[1] / d;
+        r.inner[2] = self.inner[2] / d;
+        r
+    }
+
+    pub fn translated_by(&self, x: f64, y: f64, z: f64) -> Self {
+        let new_x = self.x() + x;
+        let new_y = self.y() + y;
+        let new_z = self.z() + z;
+        Self {
+            inner: [new_x, new_y, new_z],
+        }
+    }
+
+    pub fn l1_norm(&self) -> f64 {
+        self.x() + self.y() + self.z()
+    }
+
+    pub fn l2_norm(&self) -> f64 {
+        self.l2_norm_sqd().sqrt()
+    }
+
+    pub fn l2_norm_sqd(&self) -> f64 {
+        self.x() * self.x() + self.y() * self.y() + self.z() * self.z()
+    }
+
+    pub fn cross_prod(&self, other: Self) -> Self {
+        let x: f64 = self.x() * other.y() - self.y() * other.x();
+        let y: f64 = self.y() * other.z() - self.z() * other.y();
+        let z: f64 = self.z() * other.x() - self.x() * other.z();
+        Self::new([x, y, z])
+    }
+
+    pub fn dot_prod(&self, other: Self) -> f64 {
+        self.x() * other.x() + self.y() * other.y() + self.z() * other.z()
+    }
+
+    pub fn cross_prod_magnitude_sqd(&self, other: Self) -> f64 {
+        self.cross_prod(other).l2_norm_sqd()
+    }
+
+    // fn rotated_by_3d(&self, _other: Self, _theta: f64) -> Self {
+    //     panic!("Reenable if we do 3d");
+    //     // let c  = theta.cos();
+    //     // let s = theta.sin();
+
+    //     // let cross_prod = self.cross_prod(other);
+    //     // let x = ((self.x() - other.x()*cross_prod))*(c - 1.0) + (other.z()*self.y() - other.y()*self.z())*s;
+    //     // let y = ((self.y() - other.y()*cross_prod))*(c - 1.0) + (other.x()*self.z() - other.z()*self.x())*s;
+    //     // let z = ((self.z() - other.z()*cross_prod))*(c - 1.0) + (other.y()*self.x() - other.x()*self.y())*s;
+    //     // Self::new([x,y,z])
+    // }
+}
+
+impl Add for XYZArr {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let mut r = Self::zeroes();
+        r.inner[0] = self.inner[0] + other.inner[0];
+        r.inner[1] = self.inner[1] + other.inner[1];
+        r.inner[2] = self.inner[2] + other.inner[2];
+        r
+    }
+}
+
+impl AddAssign for XYZArr {
+    fn add_assign(&mut self, other: Self) {
+        self.inner[0] += other.inner[0];
+        self.inner[1] += other.inner[1];
+        self.inner[2] += other.inner[2];
+    }
+}
+
+impl Sub for XYZArr {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        let mut r = Self::zeroes();
+        r.inner[0] = self.inner[0] - other.inner[0];
+        r.inner[1] = self.inner[1] - other.inner[1];
+        r.inner[2] = self.inner[2] - other.inner[2];
+        r
+    }
+}
+
+impl SubAssign for XYZArr {
+    fn sub_assign(&mut self, other: Self) {
+        self.inner[0] -= other.inner[0];
+        self.inner[1] -= other.inner[1];
+        self.inner[2] -= other.inner[2];
+    }
+}
+
+impl fmt::Debug for XYZArr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "({:?}, {:?}, {:?})", self.x(), self.y(), self.z())
+    }
+}
+
+impl fmt::Display for XYZArr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "({:.3}, {:.3}, {:.3})", self.x(), self.y(), self.z())
+    }
+}
